e0f4fb54d5617ab412e14e160e24536423b674eb
Ekaterina <kat.bommon@gmail.com>
2023-03-08 01:43:51 +0300

    get date of user and print date of user

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Получение данных от пользователя
+string[] GetDateOfUser (string messenge)
+{
+Console.Write(messenge);
+char[] separators =new char[] {' ',',','.'};
+string[]splitArrray=Console.ReadLine()!.Split(separators,StringSplitOptions.RemoveEmptyEntries);
+return splitArrray;
+}
+//Печать массива
+void PrintArray(string[] dateUser)
+{
+    for (int i = 0; i < dateUser.Length; i++)
+    {
+        Console.WriteLine(dateUser[i]);
+    }
+}
+PrintArray(GetDateOfUser("Введите данные -> "));
+
